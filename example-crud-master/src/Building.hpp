32fe32d425594066a41af8a30901145d04747294
Colton John Undseth <cundset2@uwo.ca>
2023-11-02 15:33:08 -0600

    Added Building class with getters and setters

---
+++ example-crud-master/src/Building.hpp
@@ -0,0 +1,42 @@
+#include <utility>
+#include <vector> 
+#include <string>
+
+class Bathroom;
+
+class Building{
+    public:
+        std::string name;
+        std::pair<float, float> location;
+        std::vector<Bathroom> bathrooms;
+
+        Building(std::string name, std::pair<float, float> loaction, std::vector<Bathroom> bathrooms){
+            this->name = name;
+            this->location = location;
+            this-> bathrooms = bathrooms;
+        }
+
+        std::string getName() const {
+            return name;
+        }
+
+        void setName(const std::string &newName) {
+            name = newName;
+        }
+
+        std::pair<float, float> getLocation() const {
+            return location;
+        }
+
+        void setLocation(const std::pair<float, float> &newLocation) {
+            location = newLocation;
+        }
+
+        void addBathroom(const Bathroom & bathroom){
+            bathrooms.push_back(bathroom);
+        }
+
+        const std::vector<Bathroom> & getBathrooms() const {
+            return bathrooms;
+        }
+};
