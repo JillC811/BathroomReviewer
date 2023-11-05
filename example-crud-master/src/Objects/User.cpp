5f19a3ce4f05d06962b37eccee575edc39e125ef
Joey <85124401+joeyc11@users.noreply.github.com>
2023-11-05 13:16:36 -0500

    added get user object

--- example-crud-master/src/Objects/User.cpp
+++ example-crud-master/src/Objects/User.cpp
@@ -9,13 +9,16 @@ User::User() {
 }
 
 User::User(oatpp::Object<UserDto> dto) {
+    id = dto->id;
     username = dto->userName;
     email = dto->email;
 }
 
 oatpp::Object<UserDto> User::convertToDto() {
     auto dto = UserDto::createShared();
+
+    dto->id = id;
     dto->userName = username.c_str();
     dto->email = email.c_str();
     return dto;
-}
+}`
--- example-crud-master/src/service/UserService.cpp
+++ example-crud-master/src/service/UserService.cpp
@@ -1,6 +1,5 @@
 
 #include "UserService.hpp"
-#include "User.hpp"
 
 
 oatpp::Object<UserDto> UserService::createUser(const oatpp::Object<UserDto>& dto) {
@@ -24,9 +23,22 @@ oatpp::Object<UserDto> UserService::updateUser(const oatpp::Object<UserDto>& dto
 
 oatpp::Object<UserDto> UserService::signIn(const oatpp::Object<signInDto>& dto) {
   auto testUser = getUserById(1);
-  User testConvert = User(testUser);
+  user testConvert = User(testUser);
   
   return testConvert.convertToDto();
+}
+
+User::Object<User> UserService::getUserObjectById(const oatpp::Int32& id, const oatpp::provider::ResourceHandle<oatpp::orm::Connection>& connection) {
+
+  auto dbResult = m_database->getUserById(id, connection);
+  OATPP_ASSERT_HTTP(dbResult->isSuccess(), Status::CODE_500, dbResult->getErrorMessage());
+  OATPP_ASSERT_HTTP(dbResult->hasMoreToFetch(), Status::CODE_404, "User not found");
+
+  auto result = dbResult->fetch<oatpp::Vector<oatpp::Object<UserDto>>>();
+  OATPP_ASSERT_HTTP(result->size() == 1, Status::CODE_500, "Unknown error");
+  user = User::User(result[0])
+  return user;
+
 }
 
 oatpp::Object<UserDto> UserService::getUserById(const oatpp::Int32& id, const oatpp::provider::ResourceHandle<oatpp::orm::Connection>& connection) {
@@ -37,7 +49,7 @@ oatpp::Object<UserDto> UserService::getUserById(const oatpp::Int32& id, const oa
 
   auto result = dbResult->fetch<oatpp::Vector<oatpp::Object<UserDto>>>();
   OATPP_ASSERT_HTTP(result->size() == 1, Status::CODE_500, "Unknown error");
-
+  
   return result[0];
 
 }
