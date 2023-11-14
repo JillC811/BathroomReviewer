dec5ca5a09a49bf500e5b7557622b70bb4635a33
Justin Tsang <jtsang87@uwo.ca>
2023-11-14 14:21:42 -0500

    no change

---
+++ build/CMakeFiles/crud-lib.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crud-lib.dir/src/ErrorHandler.cpp.o"
+  "CMakeFiles/crud-lib.dir/src/ErrorHandler.cpp.o.d"
+  "CMakeFiles/crud-lib.dir/src/Objects/User.cpp.o"
+  "CMakeFiles/crud-lib.dir/src/Objects/User.cpp.o.d"
+  "CMakeFiles/crud-lib.dir/src/service/BathroomService.cpp.o"
+  "CMakeFiles/crud-lib.dir/src/service/BathroomService.cpp.o.d"
+  "CMakeFiles/crud-lib.dir/src/service/BuildingService.cpp.o"
+  "CMakeFiles/crud-lib.dir/src/service/BuildingService.cpp.o.d"
+  "CMakeFiles/crud-lib.dir/src/service/UserService.cpp.o"
+  "CMakeFiles/crud-lib.dir/src/service/UserService.cpp.o.d"
+  "libcrud-lib.a"
+  "libcrud-lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crud-lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
