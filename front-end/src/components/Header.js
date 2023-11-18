5ad2fb8bd4bece8e89e8340ff0db9482c20dec28
eshiu2 <eshiu2@uwo.ca>
2023-11-18 16:22:16 -0500

    route to pages

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,16 +1,20 @@
 
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import { Button } from '@mui/material'
 
 import './Header.css'
 
 function Header() {
+  const navigate = useNavigate()
+
   return (
     <div class='display-block'>
       <h1>Flushable</h1>
-        <Button>Map View</Button>
-        <Button>List View</Button>
+        <Button component={Link} to="/home"> Map View </Button>
+        <Button component={Link} to="/list">List View</Button>
     </div>
   )
 }
